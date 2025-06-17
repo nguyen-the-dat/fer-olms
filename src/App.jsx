8e9f1ce51379069d0a717e0e5cf0bd61d4bbfede
Nguyen The Dat <datnthe172223@fpt.edu.vn>
2025-06-17 15:54:11 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Hello Bootstrap in React</h1>
+      <button className="btn btn-success">Click me</button>
+    </div>
+  );
+}
+
+export default App;
